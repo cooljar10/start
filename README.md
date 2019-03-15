648956c6179516aeaf83dd41bdb044b2a3ef290f
cooljar10 <34407759+cooljar10@users.noreply.github.com>
2019-03-14 19:10:35 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# start
-STARTER FOR SKIDS THAT NEEDS A PUSH ON INSTALLING
+Hello This is a nob pack it install some of the tools you will be using if you hvae question how to run them go on these group chats on kik and ask
+#learn1337
+#hackersforbtc
+good luck
+
+runs off of termux only (aka mobile hackers only)
---
+++ start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import os
+import time
+print("Made By Darkwar")
+print('\033[91m' + 'Installing please wait')
+time.sleep(2)
+os.system("pkg update && pkg upgrade")
+os.system("termux-setup-storage")
+os.system("pkg install curl php nano python python2 nmap wget git hydra apache2 neofetch fish tor")
+os.system("git clone https://github.com/xHak9x/SocialPhish")
+os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
+os.system("git clone https://github.com/sqlmapproject/sqlmap")
+print("Install complete Good luck")
+time.sleep(1)
+os.system("neofetch")
+os.system("rm -fr start.py")
